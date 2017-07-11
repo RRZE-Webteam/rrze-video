f9e2b41b8f8bc2d1e6f960f563c92aa9e8b35663
bmehler <bernhard.mehler@gmail.com>
2017-07-11 17:23:48 +0200

    - scrollTop in jquery nur auf click vom Play Button - Version 1.0.1

--- rrze-video.php
+++ rrze-video.php
@@ -4,7 +4,7 @@
 Plugin Name: RRZE Video Plugin
 Plugin URI: https://github.com/RRZE-Webteam/rrze-video
 Description: This is a video plugin to show videos on pages and in the social media footer.
-Version: 1.0.0
+Version: 1.0.1
 Author: RRZE-Webteam
 Author URI: http://blogs.fau.de/webworking/
 License: GNU GPLv2
