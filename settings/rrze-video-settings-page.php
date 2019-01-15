d12b0a8ae4c6ecd1e836db6b45a02aea2c42782a
Tom <webrocker@gmail.com>
2019-01-15 11:36:27 +0100

    [a11y] Change markup of plugin settings form; add label and button

--- settings/rrze-video-settings-page.php
+++ settings/rrze-video-settings-page.php
@@ -1,5 +1,6 @@
 <?php
 namespace RRZE\PostVideo;
+
 add_action('admin_menu', 'RRZE\PostVideo\rrze_video_plugin_admin_settings');
 function rrze_video_plugin_admin_settings() {
 
@@ -8,23 +9,18 @@ function rrze_video_plugin_admin_settings() {
 
 }
 ?>
-<?php function rrze_video_option_page(){?>
+<?php function rrze_video_option_page(){ ?>
+
 <div>
-
     <form action="options.php" method="post">
-
         <?php settings_fields('rrze_video_plugin_options');?>
         <?php do_settings_sections('rrze_video_youtube_plugin');?>
-        <p>
-            <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php esc_html_e('Änderungen speichern') ?>"  />
-        </p>
+        <p><button class="button button-primary"><?php esc_html_e('Änderungen speichern') ?></button></p>
     </form>
-
 </div>
 
 <?php } ?>
 <?php
-
 // ADMIN SETTINGS
 
 
@@ -32,7 +28,7 @@ function rrze_video_settings(){
 
     register_setting( 'rrze_video_plugin_options', 'rrze_video_plugin_options' );
     add_settings_section('plugin_main', 'RRZE Video Einstellungen', 'RRZE\PostVideo\rrze_video_section_text', 'rrze_video_youtube_plugin');
-    add_settings_field('Checkbox Element', 'Aktivieren', 'RRZE\PostVideo\rrze_video_checkbox_element_callback', 'rrze_video_youtube_plugin', 'plugin_main' );
+    add_settings_field('rrze_video_settings_yt_player', 'Aktivieren', 'RRZE\PostVideo\rrze_video_checkbox_element_callback', 'rrze_video_youtube_plugin', 'plugin_main', array('label_for'=>'rrze_video_settings_yt_player') );
 
 }
 
@@ -42,24 +38,24 @@ function rrze_video_checkbox_element_callback() {
 
     $checked = ( isset($options['youtube_activate_checkbox']) && $options['youtube_activate_checkbox'] == 1) ? 1 : 0;
 
-    $html = '<input type="checkbox" id="youtube_activate_checkbox" name="rrze_video_plugin_options[youtube_activate_checkbox]" value="1"' . checked( 1, $checked, false ) . '/>';
+    $html = '<input type="checkbox" id="rrze_video_settings_yt_player" name="rrze_video_plugin_options[youtube_activate_checkbox]" value="1"' . checked( 1, $checked, false ) . '/>';
 
     echo $html;
 }
 ?>
-<?php 
+<?php
 
 function rrze_video_section_text() {
-    
+
     echo '<p>' . esc_html_e(' Aktivierung des Youtube Players') . '</p>';
-    
+
 }
 
 
 
 function plugin_rvce_options_validate($input) {
-    
+
     $options = get_option('rrze_video_plugin_options');
     return $options;
-    
+
 }
