a898b940e76a8a426c4b700a001ad5c1a70b9eda
Tom <webrocker@gmail.com>
2019-02-25 17:15:13 +0100

    [CSS] Add markup + styles for single/archive videos full width

--- templates/rrze-video-posttype-single-template.php
+++ templates/rrze-video-posttype-single-template.php
@@ -5,17 +5,17 @@
     <div id="content">
         <div class="container">
             <div class="row">
-                <main class="entry-content">
+                <main>
                     <h1 class="screen-reader-text"><?php _e('Video aus der Mediathek'); ?></h1>
                     <?php
                         while ( have_posts() ) : the_post();
                             $url  = get_post_meta($post->ID, 'url', true);
                             $desc = get_post_meta($post->ID, 'description', true);
                     ?>
-                            <div <?php post_class('rrze-video'); ?>>
+                            <div <?php post_class('rrze-video-single'); ?>>
                                 <?php the_title('<h2>','</h2>'); ?>
                     <?php
-                            echo do_shortcode('[fauvideo id="' . $post->ID . '" width="640" showtitle="0" showinfo="1"]');
+                            echo do_shortcode('[fauvideo id="' . $post->ID . '" width="100%" showtitle="0" showinfo="1"]');
                             if( $desc != '' ){
                                 echo sprintf('<p class="rrze-video__description">%s</p>',$desc);
                             }
