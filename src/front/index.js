833126740a579eb107fb3bca2c0c18c45d9494b8
rvdforst <rolf.v.d.forst@fau.de>
2022-06-10 11:50:21 +0200

    Create index.js

---
+++ src/front/index.js
@@ -0,0 +1,8 @@
+/**
+ * RRZE-Video Plugin: Front Dependencies
+ */
+import Plyr from "plyr";
+import "plyr/src/sass/plyr.scss";
+import "./custom.scss";
+
+const players = Plyr.setup(".plyr-instance");
