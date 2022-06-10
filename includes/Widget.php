1fd95375758e4c3704b777a7ca0d539d3bdd2b69
rvdforst <rolf.v.d.forst@fau.de>
2022-06-10 11:49:09 +0200

    Update Widget.php

--- includes/Widget.php
+++ includes/Widget.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace RRZE\Video;
 
 defined('ABSPATH') || exit;
 
-use RRZE\Video\Data;
 use RRZE\Video\Player;
 
 /**
@@ -19,7 +19,7 @@ class Widget extends \WP_Widget
     {
         $widget_ops = array(
             'classname' => 'video_widget',
-            'description' => __('Zeigt Videos in der Sidebar oder im Footer an.', 'rrze-video'),
+            'description' => __('Displays videos in the sidebar or in the footer.', 'rrze-video'),
         );
         parent::__construct('video_widget', 'RRZE Video Widget', $widget_ops);
     }
@@ -62,7 +62,7 @@ class Widget extends \WP_Widget
             $arguments['show'] .= "desc";
         }
 
-        $arguments = Data::sanitize_shortcodeargs($arguments);
+        $arguments = Shortcode::instance()->sanitizeArgs($arguments, 'rrzevideo');
         if (!empty($instance['widget_title'])) {
             $arguments['widgetargs']['title'] = $instance['widget_title'];
         }
@@ -101,12 +101,7 @@ class Widget extends \WP_Widget
         $desc = isset($instance['desc']) ? true : false;
         $link = isset($instance['link']) ? true : false;
 
-
-
-
-
         $novideos = false;
-
 
         // find videos of post-type "video", use for id-selector
         $output_id_select = '';
@@ -116,9 +111,9 @@ class Widget extends \WP_Widget
         $local_videos = new \WP_Query($query_args);
         if ($local_videos->have_posts()) {
             // make a select
-            $output_id_select .= '<label for="' . esc_attr($this->get_field_id('id')) . '">' . __('Bitte wählen Sie ein Video aus Ihrer lokalen Videothek aus:', 'rrze-video') . '</label>' . PHP_EOL;
+            $output_id_select .= '<label for="' . esc_attr($this->get_field_id('id')) . '">' . __('Please select a video from your local video library:', 'rrze-video') . '</label>' . PHP_EOL;
             $output_id_select .= '<select class="widefat" id="' . esc_attr($this->get_field_id('id')) . '" name="' . $this->get_field_name('id') . '" >' . PHP_EOL;
-            $output_id_select .= '<option value="">' . __('Video aus der Mediathek auswählen', 'rrze-video') . '</option>';
+            $output_id_select .= '<option value="">' . __('Select a video from the video library', 'rrze-video') . '</option>';
             while ($local_videos->have_posts()) {
                 $local_videos->the_post();
                 $selected = (get_the_id() == esc_attr($id)) ? ' selected' : '';
@@ -126,37 +121,35 @@ class Widget extends \WP_Widget
             }
             $output_id_select .= '</select>' . PHP_EOL;
         } else {
-            $output_id_select .= __('Es sind noch keine Videos in Ihrer Mediathek vorhanden.', 'rrze-video');
+            $output_id_select .= __('There are no videos in your video library yet.', 'rrze-video');
             $novideos = true;
         }
         wp_reset_postdata();
         // : end post-type video videos select
-?>
-
-
+    ?>
         <div class="rrze-video-admin">
             <fieldset>
-                <legend><?php _e('Videoquelle', 'rrze-video'); ?></legend>
+                <legend><?php _e('Video source', 'rrze-video'); ?></legend>
                 <div class="rrze-accordeon">
-                    <p><strong><?php _e('Auswahl über URL', 'rrze-video'); ?>:</strong></p>
+                    <p><strong><?php _e('Selection via URL', 'rrze-video'); ?>:</strong></p>
 
                     <div class="remotevideo-setting">
-                        <p><?php _e('Geben Sie eine URL zum gewünschten Video an:', 'rrze-video') ?></p>
+                        <p><?php _e('Enter a URL to the video you want:', 'rrze-video') ?></p>
                         <label for="<?php echo esc_attr($this->get_field_id('url')); ?>"><?php _e('URL', 'rrze-video'); ?>:</label>
                         <input class="widefat code" id="<?php echo esc_attr($this->get_field_id('url')); ?>" name="<?php echo esc_attr($this->get_field_name('url')); ?>" type="text" value="<?php echo esc_attr($url); ?>">
 
-                        <p><em><?php _e('z. B. https://www.video.uni-erlangen.de/webplayer/id/13953', 'rrze-video') ?></em><br>
-                            Unterstützte Video-Quellen sind derzeit: Videoportal der FAU, YouTube, Vimeo.
+                        <p><em><?php _e('e.g. https://www.video.uni-erlangen.de/webplayer/id/13953', 'rrze-video') ?></em><br>
+                           <?php _e('Supported video sources are currently: FAU video portal, YouTube and Vimeo.', 'rrze-video') ?>
                         </p>
                     </div>
 
                     <?php if (!$novideos) { ?>
 
-                        <p><strong><?php _e('Alternativ Auswahl über Videothek', 'rrze-video'); ?>:</strong></p>
+                        <p><strong><?php _e('Alternative chose a video by list', 'rrze-video'); ?>:</strong></p>
                         <div class="localvideo-setting">
                             <?php echo $output_id_select; ?>
                             <p>
-                                <label for="<?php echo $this->get_field_id('genre'); ?>"><?php _e('Oder wählen Sie eine Kategorie aus Ihrer lokalen Videothek aus um aus dieser ein zufälliges Video anzuzeigen:', 'rrze-video'); ?></label>
+                                <label for="<?php echo $this->get_field_id('genre'); ?>"><?php _e('Or select a category from your local video library to display a random video from this:', 'rrze-video'); ?></label>
                                 <select class="widefat" id="<?php echo $this->get_field_id('genre'); ?>" name="<?php echo $this->get_field_name('genre'); ?>">
                                     <?php
                                     $terms = get_terms(array(
@@ -175,7 +168,7 @@ class Widget extends \WP_Widget
                                         $opts_html .= '<option value="' . $term->name . '"' . $selected . '>' . $term->name . '</option>' . PHP_EOL;
                                     }
                                     $initial_selected = ($opts_select == 0) ? ' selected' : '';
-                                    echo '<option value="0"' . $initial_selected . '>' . __('Kategorie auswählen', 'rrze-video') . '</option>' . PHP_EOL;
+                                    echo '<option value="0"' . $initial_selected . '>' . __('Choose a category', 'rrze-video') . '</option>' . PHP_EOL;
                                     echo $opts_html;
                                     ?>
                                 </select>
@@ -190,34 +183,34 @@ class Widget extends \WP_Widget
                 </div>
             </fieldset>
             <fieldset class="displayoptions">
-                <legend><?php _e('Video Optionen', 'rrze-video'); ?></legend>
+                <legend><?php _e('Video options', 'rrze-video'); ?></legend>
                 <p>
-                    <label for="<?php echo $this->get_field_id('widget_title'); ?>"><?php _e('Widget Titel:', 'rrze-video'); ?></label><br>
+                    <label for="<?php echo $this->get_field_id('widget_title'); ?>"><?php _e('Widget Title:', 'rrze-video'); ?></label><br>
                     <input class="widefat" type='text' id='<?php echo $this->get_field_id('widget_title'); ?>' name='<?php echo $this->get_field_name('widget_title'); ?>' value='<?php echo $title; ?>'>
                 </p>
                 <p>
                     <input type="checkbox" name="<?php echo $this->get_field_name('showtitle'); ?>" id="<?php echo $this->get_field_id('showtitle'); ?>" value="1" <?php checked($showtitle, 1); ?>>
-                    <label for="<?php echo $this->get_field_id('showtitle'); ?>"><?php _e('Titel anzeigen', 'rrze-video') ?></label>
-                    <br><em><?php _e('Videotitel oberhalb des Videos anzeigen; überschreibt Widget-Titel falls gesetzt.', 'rrze-video') ?></em>
+                    <label for="<?php echo $this->get_field_id('showtitle'); ?>"><?php _e('Show title', 'rrze-video') ?></label>
+                    <br><em><?php _e('Show video title above the video; override widget title if set.', 'rrze-video') ?></em>
                 </p>
                 <p>
                     <input type="checkbox" name="<?php echo $this->get_field_name('link'); ?>" id="<?php echo $this->get_field_id('link'); ?>" value="1" <?php checked($link, 1); ?>>
-                    <label for="<?php echo $this->get_field_id('link'); ?>"><?php _e('Link auf Quelle', 'rrze-video') ?></label>
-                    <br><em><?php _e('Link zur Originalquelle bei dem Videoprovider angeben', 'rrze-video') ?></em>
+                    <label for="<?php echo $this->get_field_id('link'); ?>"><?php _e('Link to source', 'rrze-video') ?></label>
+                    <br><em><?php _e('Provide a link to the original source at the video provider.', 'rrze-video') ?></em>
                 </p>
                 <p>
                     <input type="checkbox" name="<?php echo $this->get_field_name('meta'); ?>" id="<?php echo $this->get_field_id('meta'); ?>" value="1" <?php checked($meta, 1); ?>>
-                    <label for="<?php echo $this->get_field_id('meta'); ?>"><?php _e('Metaangaben anzeigen', 'rrze-video') ?></label>
-                    <br><em><?php _e('Autor, Copyright, Quelle und Beschreibung, falls angegeben', 'rrze-video') ?></em>
+                    <label for="<?php echo $this->get_field_id('meta'); ?>"><?php _e('Show metadata', 'rrze-video') ?></label>
+                    <br><em><?php _e('Author, copyright, link to source and description if given.', 'rrze-video') ?></em>
                 </p>
                 <p>
                     <input type="checkbox" name="<?php echo $this->get_field_name('info'); ?>" id="<?php echo $this->get_field_id('info'); ?>" value="1" <?php checked($info, 1); ?>>
-                    <label for="<?php echo $this->get_field_id('info'); ?>"><?php _e('Vollständige Info angeben', 'rrze-video') ?></label>
-                    <br><em><?php _e('Metaangaben, Link und Beschreibung ausgeben', 'rrze-video') ?></em>
+                    <label for="<?php echo $this->get_field_id('info'); ?>"><?php _e('Show complete information', 'rrze-video') ?></label>
+                    <br><em><?php _e('Metadata, link to source and description if given.', 'rrze-video') ?></em>
                 </p>
             </fieldset>
         </div>
-<?php
+    <?php
     }
 
     /*
@@ -237,7 +230,6 @@ class Widget extends \WP_Widget
         $instance['link']           = $new_instance['link'];
         $instance['info']           = $new_instance['info'];
         $instance['genre']          = strip_tags($new_instance['genre']);
-
 
         return $instance;
     }
