67879c6daee653ca441835bc9a19fffa5a65e42f
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-07-24 13:58:07 +0200

    v3.5.2 Build

--- build/blocks/index.asset.php
+++ build/blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '5941b31e4b18c712aed8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c480baa12d80d3f9c90e');
