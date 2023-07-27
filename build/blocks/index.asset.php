aee228a997b5f945fc2fc33b7143a2ce6e157112
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-07-27 13:50:01 +0200

    Add basic Blockeditor support

--- build/blocks/index.asset.php
+++ build/blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c480baa12d80d3f9c90e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'df512697496696bb8ba9');
