0009afe7a71936c2482be553018272644e40bbef
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-07-28 08:49:39 +0200

    v3.5.4 - Wait for the dom

--- src/front/index.js
+++ src/front/index.js
@@ -5,27 +5,29 @@ import Plyr from "plyr";
 import "plyr/src/sass/plyr.scss";
 import "./custom.scss";
 
-try{
-const players = Plyr.setup(".plyr-instance");
+document.addEventListener("DOMContentLoaded", function () {
+  try {
+    const players = Plyr.setup(".plyr-instance");
 
-players.forEach((player, index) => {
-    index = index + 2;
+    players.forEach((player, index) => {
+      index = index + 2;
 
-    player.on('play', function() {
+      player.on("play", function () {
         let videoTitle = document.getElementById(`rrze-video-title-${index}`);
         if (videoTitle !== null) {
-            videoTitle.classList.add('rrze-video-hide');
+          videoTitle.classList.add("rrze-video-hide");
         }
-    });
+      });
 
-    player.on('pause', function() {
+      player.on("pause", function () {
         let videoTitle = document.getElementById(`rrze-video-title-${index}`);
         if (videoTitle !== null) {
-            videoTitle.classList.remove('rrze-video-hide');
-            videoTitle.style.opacity = '1'; // Reset opacity to 1 immediately to make fadeIn work correctly
+          videoTitle.classList.remove("rrze-video-hide");
+          videoTitle.style.opacity = "1"; // Reset opacity to 1 immediately to make fadeIn work correctly
         }
+      });
     });
+  } catch (error) {
+    console.error("Error in rrze-video/src/front/index.js: ", error);
+  }
 });
-} catch (error) {
-    console.error('Error in rrze-video/src/front/index.js: ', error);
-}
