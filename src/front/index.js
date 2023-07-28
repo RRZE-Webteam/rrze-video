acd6ae1f8ddccfb0bf1b9acde0387112f00d424d
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-07-28 10:53:55 +0200

    Fix JS

--- src/front/index.js
+++ src/front/index.js
@@ -6,25 +6,23 @@ import "plyr/src/sass/plyr.scss";
 import "./custom.scss";
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.addEventListener("DOMContentLoaded", function () {
     try {
       const players = Plyr.setup(".plyr-instance");
-
+  
       players.forEach((player) => {
-        let videoTitleId =
-          player.elements?.container?.nextSibling?.nextSibling?.id;
+        let videoTitleId = player.elements?.container?.nextSibling?.nextSibling?.id;
         let videoTitle = document.getElementById(videoTitleId);
-
+        
         if (videoTitle === null) {
           return;
         }
-
+        
         videoTitle.classList.remove("rrze-video-hide");
-
+  
         player.on("play", function () {
           videoTitle.classList.add("rrze-video-hide");
         });
-
+  
         player.on("pause", function () {
           videoTitle.classList.remove("rrze-video-hide");
           videoTitle.style.opacity = "1"; // Reset opacity to 1 immediately to make fadeIn work correctly
@@ -33,5 +31,4 @@ document.addEventListener("DOMContentLoaded", function () {
     } catch (error) {
       console.error("Error in rrze-video/src/front/index.js: ", error);
     }
-  });
 });
