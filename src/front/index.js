b6f30694a8f73c0225c17dde910a7098f2bdd817
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-07-28 09:51:27 +0200

    Fix the videotitle on pages with multiple sources

--- src/front/index.js
+++ src/front/index.js
@@ -6,28 +6,32 @@ import "plyr/src/sass/plyr.scss";
 import "./custom.scss";
 
 document.addEventListener("DOMContentLoaded", function () {
-  try {
-    const players = Plyr.setup(".plyr-instance");
+  document.addEventListener("DOMContentLoaded", function () {
+    try {
+      const players = Plyr.setup(".plyr-instance");
 
-    players.forEach((player, index) => {
-      index = index + 2;
+      players.forEach((player) => {
+        let videoTitleId =
+          player.elements?.container?.nextSibling?.nextSibling?.id;
+        let videoTitle = document.getElementById(videoTitleId);
 
-      player.on("play", function () {
-        let videoTitle = document.getElementById(`rrze-video-title-${index}`);
-        if (videoTitle !== null) {
-          videoTitle.classList.add("rrze-video-hide");
+        if (videoTitle === null) {
+          return;
         }
-      });
 
-      player.on("pause", function () {
-        let videoTitle = document.getElementById(`rrze-video-title-${index}`);
-        if (videoTitle !== null) {
+        videoTitle.classList.remove("rrze-video-hide");
+
+        player.on("play", function () {
+          videoTitle.classList.add("rrze-video-hide");
+        });
+
+        player.on("pause", function () {
           videoTitle.classList.remove("rrze-video-hide");
           videoTitle.style.opacity = "1"; // Reset opacity to 1 immediately to make fadeIn work correctly
-        }
+        });
       });
-    });
-  } catch (error) {
-    console.error("Error in rrze-video/src/front/index.js: ", error);
-  }
+    } catch (error) {
+      console.error("Error in rrze-video/src/front/index.js: ", error);
+    }
+  });
 });
