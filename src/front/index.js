300d02d5ad29d1203b48afc641909da3278802ce
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-08-07 15:12:55 +0200

    Add IOS Native Support and Caption support for IOS Devices

--- src/front/index.js
+++ src/front/index.js
@@ -5,9 +5,11 @@ import Plyr from "plyr";
 import "plyr/src/sass/plyr.scss";
 import "./custom.scss";
 
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
   try {
-    const players = Plyr.setup(".plyr-instance");
+    const players = Plyr.setup(".plyr-instance", {
+      fullscreen: { iosNative: true },
+    });
 
     players.forEach((player) => {
       let videoTitleId =
@@ -28,8 +30,26 @@ window.onload = function () {
         videoTitle.classList.remove("rrze-video-hide");
         videoTitle.style.opacity = "1"; // Reset opacity to 1 immediately to make fadeIn work correctly
       });
+
+      if (/(iPhone|iPod|iPad).*AppleWebKit/i.test(navigator.userAgent)) {
+        ["webkitbeginfullscreen", "webkitendfullscreen"].forEach((event) => {
+          player.media.addEventListener(event, (e) => {
+            if (e.type === "webkitbeginfullscreen") {
+              document.documentElement.style.setProperty(
+                "--webkit-text-track-display",
+                "block"
+              );
+            } else {
+              document.documentElement.style.setProperty(
+                "--webkit-text-track-display",
+                "none"
+              );
+            }
+          });
+        });
+      }
     });
   } catch (error) {
     console.error("Error in rrze-video/src/front/index.js: ", error);
   }
-};
+});
