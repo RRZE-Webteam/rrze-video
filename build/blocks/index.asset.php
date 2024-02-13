daa8fe832796812cda8ea442812e8bbd7f2e4b57
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-02-13 10:39:55 +0100

    feat(block): Add block support
    Adds support for secure embed, looping and start values via the interface.

--- build/blocks/index.asset.php
+++ build/blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '65e2b2973aa1feb8727c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ea5f865af2f95f6d41f5');
