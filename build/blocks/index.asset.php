5120c692e0af241d1e6ac8af1a26803de66e218a
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-02-15 09:25:47 +0100

    fix(blockUI): Refines the UX inside the block UI
    - Adds Button Indicators if values can be updated
    - Disables unnecessary PanelBodies based on the Videoprovider

--- build/blocks/index.asset.php
+++ build/blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ea5f865af2f95f6d41f5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '5001bdcb78dd4f2a8f28');
